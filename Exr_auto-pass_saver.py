0288171cace1a9bbac8a5d284bc4a764116cd15c
Christian Kandler <15774117+ck3dio@users.noreply.github.com>
2025-12-12 16:48:40 +0100

    Update for Blender 5

--- Exr_auto-pass_saver.py
+++ Exr_auto-pass_saver.py
@@ -4,9 +4,8 @@ bl_info = {
     "blender": (2, 80, 0),
     "category": "Compositing",
     "author": "3d-io",
-    "version": (1, 0),
+    "version": (1, 1, 0),
     "location": "Compositor Tab > Sidebar > Exr Auto Pass Saver",
-    "warning": "BSD",
     "wiki_url": "https://github.com/3d-io/",
     "support": "COMMUNITY",
 }
@@ -33,11 +32,18 @@ class Exr_Auto_Pass_Saver(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
 
+    def GetNodeTree(self):
+        if bpy.app.version[0] >= 5:
+            return bpy.context.scene.compositing_node_group
+        else:
+            return bpy.context.scene.node_tree
+
+
     # removes all currently existing nodes
     def cleannodes(self):
-        nodesField = bpy.context.scene.node_tree
-        for currentNode in nodesField.nodes:
-            nodesField.nodes.remove(currentNode)
+        nodeTree = self.GetNodeTree()
+        for currentNode in nodeTree.nodes:
+            nodeTree.nodes.remove(currentNode)
 
 
     # opens the directory of the current render path
@@ -46,8 +52,8 @@ class Exr_Auto_Pass_Saver(bpy.types.Operator):
         subprocess.call("explorer " + path, shell=True)
 
 
-    # returns a target render path taken from the scene's render output and cleaned up as needed
-    def GetOutputPathStr(self):
+    # sets target render path taken from the scene's render output and cleaned up as needed
+    def SetOutputPath(self, node):
         defaultPath = bpy.context.scene.render.filepath
         dirname = os.path.dirname(defaultPath)
         if not dirname.endswith( ('/', '\\') ):
@@ -55,23 +61,29 @@ class Exr_Auto_Pass_Saver(bpy.types.Operator):
         if not dirname:
             dirname = os.path.expanduser('~') + "/"
         fileName = "output"
-        fullPath = dirname + fileName
-        return fullPath
+        if bpy.app.version[0] >= 5:
+            node.directory = dirname
+            node.file_name = fileName
+        else:
+            node.base_path = dirname + fileName
 
 
     # creates a new Render Layers node at the given position
     def CreateNodeRenderLayers(self, position):
-        node = bpy.context.scene.node_tree.nodes.new('CompositorNodeRLayers')
+        nodeTree = self.GetNodeTree()
+        node = nodeTree.nodes.new('CompositorNodeRLayers')
         node.location = position
         return node
 
 
     # creates a new Output File node at the given position
     def CreateNodeFileOutput(self, position):
+        bpy.context.scene.render.image_settings.media_type = 'MULTI_LAYER_IMAGE'
         bpy.context.scene.render.image_settings.file_format = 'OPEN_EXR_MULTILAYER'
-        node = bpy.context.scene.node_tree.nodes.new("CompositorNodeOutputFile")
+        nodeTree = self.GetNodeTree()
+        node = nodeTree.nodes.new("CompositorNodeOutputFile")
         node.label = 'EXR-MultiLayer'
-        node.base_path = self.GetOutputPathStr()
+        self.SetOutputPath(node)
         node.location = position
         node.width = 300
         node.use_custom_color = True
@@ -81,6 +93,7 @@ class Exr_Auto_Pass_Saver(bpy.types.Operator):
 
     # links all outputs of the source node to inputs of the target node
     def LinkRenderLayers(self, sourceNode, targetNode):
+        nodeTree = self.GetNodeTree()
         for out in sourceNode.outputs:
             # skip disabled outputs
             if (out.enabled == False):
@@ -91,13 +104,20 @@ class Exr_Auto_Pass_Saver(bpy.types.Operator):
                 if (src.identifier == out.identifier):
                     # target node already has matching input, link to it
                     found = True
-                    bpy.context.scene.node_tree.links.new(out, targetNode.inputs[slot])
+                    nodeTree.links.new(out, targetNode.inputs[slot])
                     break
                 slot = slot + 1
             if not found:
                 # target node has no matching input, create one and link to it
-                targetNode.file_slots.new(out.identifier)
-                bpy.context.scene.node_tree.links.new(out, targetNode.inputs[-1])
+                if bpy.app.version[0] >= 5:
+                    socketType = out.type
+                    if socketType == 'VALUE':
+                        socketType = 'FLOAT'
+                    targetNode.file_output_items.new(socketType, out.identifier)
+                    nodeTree.links.new(out, targetNode.inputs[-2])
+                else:
+                    targetNode.file_slots.new(out.identifier)
+                    nodeTree.links.new(out, targetNode.inputs[-1])
 
 
     # Exr Auto Saver button
@@ -105,7 +125,8 @@ class Exr_Auto_Pass_Saver(bpy.types.Operator):
     def execute(self, context):
         scene = bpy.context.scene # get the scene
 
-        if not scene.use_nodes:
+        #if not scene.use_nodes:
+        if self.GetNodeTree() == None:
             print('Info: node_tree not in use. Skipping')
             return {'CANCELLED'}
 
@@ -131,15 +152,34 @@ class Exr_Auto_Pass_Saver_Panel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         layout.label(text="Link all nodes:")
-        # draw Auto Saver button
+
         row = layout.row()
         row.scale_y = 2.0
         row.operator(Exr_Auto_Pass_Saver.bl_idname, icon='TRACKING_FORWARDS')
         sce = context.scene
-        # draw the checkbox (implied from property type = bool)
+
         layout.prop(sce, "exr_auto_pass_saver_clear_all") 
         layout.prop(sce, "exr_auto_pass_saver_open_dir") 
 
+        layout.separator()
+        box = layout.box()
+        col = box.column(align=True)
+        row = col.row()
+        row.label(text="Our Other Tools", icon='INFO')
+        col.separator(factor=0.5)
+
+        row = col.row()
+        row.label(text="Free EXR reader for PS", icon='BLANK1')
+        row = col.row()
+        row.scale_y = 1.2
+        row.operator("wm.url_open", text="Exr-IO", icon="URL").url = "https://www.exr-io.com"
+        col.separator(factor=0.3)
+
+        row = col.row()
+        row.label(text="Need auto unwrapping?", icon='BLANK1')
+        row = col.row()
+        row.scale_y = 1.2
+        row.operator("wm.url_open", text="Unwrella", icon="URL").url = "https://www.unwrella.com"
 
 classes = (Exr_Auto_Pass_Saver_Panel, Exr_Auto_Pass_Saver)
 
